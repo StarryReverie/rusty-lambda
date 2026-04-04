cee0400f9f5c25513d592809f72542e7d7034f65
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-04-04 12:53:03 +0800

    refactor(base): use type alias instead of newtype for curryed functions

--- src/base/function/currying.rs
+++ src/base/function/currying.rs
@@ -3,183 +3,92 @@ use std::sync::Arc;
 use crate::base::function::WrappedFn;
 use crate::base::value::{StaticConcurrent, Value};
 
-macro_rules! impl_curryed_fn {
-    ($name:ident < $($ty:ident),* >, $first:ident, $output:ty) => {
-        impl<$($ty),*> $crate::base::function::ConcurrentFn<$first> for $name<$($ty),*>
-        // where
-        //     $($ty: 'static,)*
+pub trait Curry<D, F> {
+    fn curry(func: F) -> Self;
+}
+
+pub type Curryed1Fn<T1, R> = WrappedFn<T1, R>;
+pub type Curryed2Fn<T1, T2, R> = WrappedFn<T1, Curryed1Fn<T2, R>>;
+pub type Curryed3Fn<T1, T2, T3, R> = WrappedFn<T1, Curryed2Fn<T2, T3, R>>;
+pub type Curryed4Fn<T1, T2, T3, T4, R> = WrappedFn<T1, Curryed3Fn<T2, T3, T4, R>>;
+pub type Curryed5Fn<T1, T2, T3, T4, T5, R> = WrappedFn<T1, Curryed4Fn<T2, T3, T4, T5, R>>;
+pub type Curryed6Fn<T1, T2, T3, T4, T5, T6, R> = WrappedFn<T1, Curryed5Fn<T2, T3, T4, T5, T6, R>>;
+pub type Curryed7Fn<T1, T2, T3, T4, T5, T6, T7, R> =
+    WrappedFn<T1, Curryed6Fn<T2, T3, T4, T5, T6, T7, R>>;
+pub type Curryed8Fn<T1, T2, T3, T4, T5, T6, T7, T8, R> =
+    WrappedFn<T1, Curryed7Fn<T2, T3, T4, T5, T6, T7, T8, R>>;
+
+macro_rules! impl_curry {
+    (@closure $func:ident, [$($captured:ident),*], $T:ident $x:ident) => {
+        WrappedFn::from(move |$x: $T| -> R {
+            $(let $captured = $captured.clone();)*
+            (Arc::clone(&$func))($($captured,)* $x)
+        })
+    };
+
+    (@closure $func:ident, [$($captured:ident),*], $T:ident $x:ident, $($rest_T:ident $rest_x:ident),+) => {
+        WrappedFn::from(move |$x: $T| {
+            $(let $captured = $captured.clone();)*
+            let $func = Arc::clone(&$func);
+            impl_curry!(@closure $func, [$($captured,)* $x], $($rest_T $rest_x),+)
+        })
+    };
+
+    ($type:ident, $($T:ident $x:ident),+) => {
+        impl<$($T,)+ R, F> Curry<($($T,)+ R), F> for $type<$($T,)+ R>
+        where
+            $($T: Value + 'static,)+
+            F: Fn($($T),+) -> R + StaticConcurrent,
         {
-            type Output = $output;
-
-            fn call(&self, argument: $first) -> Self::Output {
-                self.0.call(argument)
-            }
-        }
-
-        impl<$($ty),*> $crate::base::value::Value for $name<$($ty),*> {
-            type Unwrapped = Self;
-
-            type View<'a>
-                = &'a <Self as ::std::ops::Deref>::Target
-            where
-                Self: 'a;
-
-            fn make<U>(unwrapped: U) -> Self
-            where
-                U: ::std::convert::Into<Self::Unwrapped>,
-                Self::Unwrapped: ::std::marker::Sized,
-            {
-                unwrapped.into()
-            }
-
-            fn view(&self) -> Self::View<'_> {
-                &**self
-            }
-        }
-
-        impl<$($ty),*> ::std::ops::Deref for $name<$($ty),*> {
-            type Target = dyn ::std::ops::Fn($first) -> $output + ::std::marker::Send + ::std::marker::Sync;
-
-            fn deref(&self) -> &Self::Target {
-                &*self.0
-            }
-        }
-
-        impl<$($ty),*> ::std::clone::Clone for $name<$($ty),*> {
-            fn clone(&self) -> Self {
-                Self(self.0.clone())
+            fn curry(func: F) -> Self {
+                let func = Arc::new(func);
+                impl_curry!(@closure func, [], $($T $x),+)
             }
         }
     };
 }
 
-pub struct Curryed1Fn<T1, R>(WrappedFn<T1, R>);
-
-impl_curryed_fn!(Curryed1Fn<T1, R>, T1, R);
-
-impl<T1, R, F> From<F> for Curryed1Fn<T1, R>
+impl<T1, R, F> Curry<(T1, R), F> for Curryed1Fn<T1, R>
 where
+    T1: Value + 'static,
     F: Fn(T1) -> R + StaticConcurrent,
 {
-    fn from(func: F) -> Self {
-        Self(WrappedFn::from(func))
+    fn curry(func: F) -> Self {
+        WrappedFn::from(func)
     }
 }
 
-pub struct Curryed2Fn<T1, T2, R>(WrappedFn<T1, Curryed1Fn<T2, R>>);
-
-impl_curryed_fn!(Curryed2Fn<T1, T2, R>, T1, Curryed1Fn<T2, R>);
-
-impl<T1, T2, R, F> From<F> for Curryed2Fn<T1, T2, R>
-where
-    T1: Value + 'static,
-    F: Fn(T1, T2) -> R + StaticConcurrent,
-{
-    fn from(func: F) -> Self {
-        let func = Arc::new(func);
-        Self(WrappedFn::from(move |x1: T1| -> Curryed1Fn<T2, R> {
-            let func = Arc::clone(&func);
-            Curryed1Fn(WrappedFn::from(move |x2: T2| -> R {
-                let x1 = x1.clone();
-                (Arc::clone(&func))(x1, x2)
-            }))
-        }))
-    }
-}
-
-pub struct Curryed3Fn<T1, T2, T3, R>(WrappedFn<T1, Curryed2Fn<T2, T3, R>>);
-
-impl_curryed_fn!(Curryed3Fn<T1, T2, T3, R>, T1, Curryed2Fn<T2, T3, R>);
-
-impl<T1, T2, T3, R, F> From<F> for Curryed3Fn<T1, T2, T3, R>
-where
-    T1: Value + 'static,
-    T2: Value + 'static,
-    F: Fn(T1, T2, T3) -> R + StaticConcurrent,
-{
-    fn from(func: F) -> Self {
-        let func = Arc::new(func);
-        Self(WrappedFn::from(move |x1: T1| -> Curryed2Fn<T2, T3, R> {
-            let func = Arc::clone(&func);
-            Curryed2Fn(WrappedFn::from(move |x2: T2| -> Curryed1Fn<T3, R> {
-                let x1 = x1.clone();
-                let func = Arc::clone(&func);
-                Curryed1Fn(WrappedFn::from(move |x3: T3| -> R {
-                    let (x1, x2) = (x1.clone(), x2.clone());
-                    (Arc::clone(&func))(x1, x2, x3)
-                }))
-            }))
-        }))
-    }
-}
-
-pub struct Curryed4Fn<T1, T2, T3, T4, R>(WrappedFn<T1, Curryed3Fn<T2, T3, T4, R>>);
-
-impl_curryed_fn!(Curryed4Fn<T1, T2, T3, T4, R>, T1, Curryed3Fn<T2, T3, T4, R>);
-
-impl<T1, T2, T3, T4, R, F> From<F> for Curryed4Fn<T1, T2, T3, T4, R>
-where
-    T1: Value + 'static,
-    T2: Value + 'static,
-    T3: Value + 'static,
-    F: Fn(T1, T2, T3, T4) -> R + StaticConcurrent,
-{
-    fn from(func: F) -> Self {
-        let func = Arc::new(func);
-        Self(WrappedFn::from(
-            move |x1: T1| -> Curryed3Fn<T2, T3, T4, R> {
-                let func = Arc::clone(&func);
-                Curryed3Fn(WrappedFn::from(move |x2: T2| -> Curryed2Fn<T3, T4, R> {
-                    let x1 = x1.clone();
-                    let func = Arc::clone(&func);
-                    Curryed2Fn(WrappedFn::from(move |x3: T3| -> Curryed1Fn<T4, R> {
-                        let (x1, x2) = (x1.clone(), x2.clone());
-                        let func = Arc::clone(&func);
-                        Curryed1Fn(WrappedFn::from(move |x4: T4| -> R {
-                            let (x1, x2, x3) = (x1.clone(), x2.clone(), x3.clone());
-                            (Arc::clone(&func))(x1, x2, x3, x4)
-                        }))
-                    }))
-                }))
-            },
-        ))
-    }
-}
+impl_curry!(Curryed2Fn, T1 x1, T2 x2);
+impl_curry!(Curryed3Fn, T1 x1, T2 x2, T3 x3);
+impl_curry!(Curryed4Fn, T1 x1, T2 x2, T3 x3, T4 x4);
+impl_curry!(Curryed5Fn, T1 x1, T2 x2, T3 x3, T4 x4, T5 x5);
+impl_curry!(Curryed6Fn, T1 x1, T2 x2, T3 x3, T4 x4, T5 x5, T6 x6);
+impl_curry!(Curryed7Fn, T1 x1, T2 x2, T3 x3, T4 x4, T5 x5, T6 x6, T7 x7);
+impl_curry!(Curryed8Fn, T1 x1, T2 x2, T3 x3, T4 x4, T5 x5, T6 x6, T7 x7, T8 x8);
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_curryed1fn() {
-        let f: Curryed1Fn<i32, i32> = Curryed1Fn::from(|x: i32| x * 2);
-        assert_eq!(f(3), 6);
-    }
-
-    #[test]
-    fn test_curryed2fn() {
-        let f: Curryed2Fn<i32, i32, i32> = Curryed2Fn::from(|x, y| x + y);
-        assert_eq!(f(1)(2), 3);
-    }
-
-    #[test]
-    fn test_curryed3fn() {
-        let f: Curryed3Fn<i32, i32, i32, i32> = Curryed3Fn::from(|x, y, z| x * y + z);
-        assert_eq!(f(2)(3)(1), 7);
-    }
-
-    #[test]
     fn test_curryed4fn() {
-        let f: Curryed4Fn<i32, i32, i32, i32, i32> = Curryed4Fn::from(|a, b, c, d| a + b * c - d);
+        let f = WrappedFn::curry(|a, b, c, d| a + b * c - d);
         assert_eq!(f(1)(2)(3)(4), 3);
     }
 
     #[test]
     fn test_curryed4fn_clone_independence() {
-        let f: Curryed4Fn<i32, i32, i32, i32, i32> =
-            Curryed4Fn::from(|a, b, c, d| a * 1000 + b * 100 + c * 10 + d);
+        let f = WrappedFn::curry(|a, b, c, d| a * 1000 + b * 100 + c * 10 + d);
         let f2 = f.clone();
         assert_eq!(f(1)(2)(3)(4), 1234);
         assert_eq!(f2(5)(6)(7)(8), 5678);
+    }
+
+    #[test]
+    fn test_nested_curryedfn() {
+        let f = WrappedFn::curry(|a: i32, b: i32| {
+            WrappedFn::curry(move |c: i32, d: i32| a + b + c + d)
+        });
+        assert_eq!(f(1)(2)(3)(4), 10);
     }
 }
--- src/base/function/mod.rs
+++ src/base/function/mod.rs
@@ -1,5 +1,8 @@
 mod currying;
 mod wrapper;
 
-pub use currying::{Curryed1Fn, Curryed2Fn, Curryed3Fn, Curryed4Fn};
+pub use currying::{
+    Curry, Curryed1Fn, Curryed2Fn, Curryed3Fn, Curryed4Fn, Curryed5Fn, Curryed6Fn, Curryed7Fn,
+    Curryed8Fn,
+};
 pub use wrapper::{ConcurrentFn, WrappedFn};

b344f8a4f6c04def3d47782348e03be09a5b06f1
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-04-05 20:31:54 +0800

    feat(data): implement List as Foldable instance and add tests

--- src/control/structure/foldable/typeclass.rs
+++ src/control/structure/foldable/typeclass.rs
@@ -3,9 +3,9 @@ use std::ops::ControlFlow;
 use crate::base::function::{ConcurrentFn, Curry, WrappedFn};
 use crate::base::hkt::TypeConstructor1;
 use crate::base::value::Value;
+use crate::control::structure::monoid::Monoid;
 use crate::control::structure::monoid::all::All;
 use crate::control::structure::monoid::any::Any;
-use crate::control::structure::monoid::Monoid;
 use crate::control::structure::semigroup::Semigroup;
 use crate::data::list::List;
 use crate::data::maybe::Maybe;
@@ -163,5 +163,149 @@ pub trait Foldable: TypeConstructor1 {
         F: for<'a> Value<View<'a>: ConcurrentFn<A, Output = List<B>>>,
     {
         Self::fold_map(map, container)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::base::function::{Curry, WrappedFn};
+    use crate::data::list::{List, ListInstance};
+
+    use super::*;
+
+    #[test]
+    fn test_try_foldr_short_circuits() {
+        let xs = List::from(vec![1, 2, 3, 4, 5]);
+        let result = ListInstance::try_foldr(
+            WrappedFn::curry(|x, a| x + a),
+            |x| {
+                if x == 3 {
+                    ControlFlow::Break(x)
+                } else {
+                    ControlFlow::Continue(x)
+                }
+            },
+            0,
+            xs,
+        );
+        assert_eq!(result, 6);
+    }
+
+    #[test]
+    fn test_foldr() {
+        let sum = ListInstance::foldr(WrappedFn::curry(|x: i32, a: i32| x + a), 0, List::empty());
+        assert_eq!(sum, 0);
+
+        let xs = List::from(vec![1, 2, 3]);
+        let sum = ListInstance::foldr(WrappedFn::curry(|x, a| x + a), 0, xs);
+        assert_eq!(sum, 6);
+    }
+
+    #[test]
+    fn test_foldr_cons_order() {
+        let xs = List::from(vec![1, 2, 3]);
+        let result = ListInstance::foldr(WrappedFn::curry(|x, a| x + a * 10), 0, xs);
+        assert_eq!(result, 321);
+    }
+
+    #[test]
+    fn test_foldr1() {
+        let result = ListInstance::foldr1(WrappedFn::curry(|x: i32, a: i32| x + a), List::empty());
+        assert_eq!(result, Maybe::Nothing);
+
+        let xs = List::from(vec![1, 2, 3]);
+        let result = ListInstance::foldr1(WrappedFn::curry(|x, a| x + a), xs);
+        assert_eq!(result, Maybe::Just(6));
+    }
+
+    #[test]
+    fn test_foldr1_cons_order() {
+        let xs = List::from(vec![1, 2, 3]);
+        let result = ListInstance::foldr1(WrappedFn::curry(|x, a| x + a * 10), xs);
+        assert_eq!(result, Maybe::Just(321));
+    }
+
+    #[test]
+    fn test_to_list() {
+        let xs = List::from(vec![1, 2, 3]);
+        assert_eq!(ListInstance::to_list(xs), List::from(vec![1, 2, 3]));
+        assert_eq!(ListInstance::to_list(List::<i32>::empty()), List::empty());
+    }
+
+    #[test]
+    fn test_null() {
+        assert!(ListInstance::null(List::<i32>::empty()));
+        assert!(!ListInstance::null(List::singleton(1)));
+        assert!(!ListInstance::null(List::from(vec![1, 2, 3])));
+    }
+
+    #[test]
+    fn test_length() {
+        assert_eq!(ListInstance::length(List::<i32>::empty()), 0);
+        assert_eq!(ListInstance::length(List::singleton(1)), 1);
+        assert_eq!(ListInstance::length(List::from(vec![1, 2, 3])), 3);
+    }
+
+    #[test]
+    fn test_any_all() {
+        let xs = List::from(vec![1, 2, 3, 4, 5]);
+        assert!(ListInstance::any(|x: &i32| *x == 3, xs.clone()));
+        assert!(!ListInstance::any(|x: &i32| *x > 10, xs.clone()));
+        assert!(ListInstance::all(|x: &i32| *x > 0, xs.clone()));
+        assert!(!ListInstance::all(|x: &i32| *x > 2, xs));
+    }
+
+    #[test]
+    fn test_find() {
+        let xs = List::from(vec![1, 2, 3, 4, 5]);
+        assert_eq!(
+            ListInstance::find(|x: &i32| *x == 3, xs.clone()),
+            Maybe::Just(3)
+        );
+        assert_eq!(ListInstance::find(|x: &i32| *x == 99, xs), Maybe::Nothing);
+        assert_eq!(
+            ListInstance::find(|x: &i32| *x == 42, List::empty()),
+            Maybe::Nothing
+        );
+    }
+
+    #[test]
+    fn test_elem() {
+        let xs = List::from(vec![1, 2, 3]);
+        assert!(ListInstance::elem(2, xs.clone()));
+        assert!(!ListInstance::elem(5, xs));
+        assert!(!ListInstance::elem(1, List::empty()));
+    }
+
+    #[test]
+    fn test_and_or() {
+        assert!(ListInstance::and(List::from(vec![true, true, true])));
+        assert!(!ListInstance::and(List::from(vec![true, false, true])));
+        assert!(ListInstance::and(List::empty()));
+        assert!(ListInstance::or(List::from(vec![false, true, false])));
+        assert!(!ListInstance::or(List::from(vec![false, false, false])));
+        assert!(!ListInstance::or(List::empty()));
+    }
+
+    #[test]
+    fn test_concat() {
+        let xs = List::from(vec![
+            List::from(vec![1, 2]),
+            List::empty(),
+            List::from(vec![3, 4, 5]),
+        ]);
+        assert_eq!(ListInstance::concat(xs), List::from(vec![1, 2, 3, 4, 5]));
+        assert_eq!(
+            ListInstance::concat(List::<List<i32>>::empty()),
+            List::empty()
+        );
+    }
+
+    #[test]
+    fn test_concat_map() {
+        let xs = List::from(vec![1, 2, 3]);
+        let result =
+            ListInstance::concat_map(WrappedFn::from(|x: i32| List::from(vec![x, x * 10])), xs);
+        assert_eq!(result, List::from(vec![1, 10, 2, 20, 3, 30]));
     }
 }
---
+++ src/data/list/foldable.rs
@@ -0,0 +1,78 @@
+use std::ops::ControlFlow;
+
+use crate::base::function::ConcurrentFn;
+use crate::base::hkt::TypeConstructor1;
+use crate::base::value::Value;
+use crate::control::structure::foldable::Foldable;
+use crate::data::list::ListInstance;
+use crate::data::maybe::Maybe;
+
+impl Foldable for ListInstance {
+    fn try_foldr<A, B, F, T>(accum: F, try_break: T, init: B, container: Self::Type<A>) -> B
+    where
+        A: Value,
+        B: Value,
+        F: for<'a> Value<View<'a>: ConcurrentFn<A, Output: ConcurrentFn<B, Output = B>>>,
+        T: ConcurrentFn<A, Output = ControlFlow<B, A>>,
+    {
+        fn foldr_impl<A: Value, B: Value>(
+            accum: &impl for<'a> Value<View<'a>: ConcurrentFn<A, Output: ConcurrentFn<B, Output = B>>>,
+            try_break: &impl ConcurrentFn<A, Output = ControlFlow<B, A>>,
+            init: B,
+            container: <ListInstance as TypeConstructor1>::Type<A>,
+        ) -> B {
+            match container.decompose() {
+                Maybe::Just((x, xs)) => match try_break.call(x) {
+                    ControlFlow::Break(res) => res,
+                    ControlFlow::Continue(x) => {
+                        let res = foldr_impl(accum, try_break, init, xs);
+                        accum.view().call(x).call(res)
+                    }
+                },
+                Maybe::Nothing => init,
+            }
+        }
+        foldr_impl(&accum, &try_break, init, container)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::base::function::{ConcurrentFn, Curry, WrappedFn};
+    use crate::control::structure::foldable::Foldable;
+    use crate::control::structure::functor::Functor;
+    use crate::data::list::List;
+    use crate::data::list::ListInstance;
+
+    #[test]
+    fn test_foldable_fold_fold_map_id_law() {
+        let xs = List::from(vec![
+            List::from(vec![1]),
+            List::from(vec![2, 3]),
+            List::empty(),
+            List::from(vec![4, 5]),
+        ]);
+        let lhs = ListInstance::fold(xs.clone());
+        let rhs = ListInstance::fold_map(WrappedFn::from(|x| x), xs);
+        assert_eq!(lhs, rhs);
+    }
+
+    #[test]
+    fn test_foldable_fold_map_fmap_fusion_law() {
+        let xs = List::from(vec![1, 2, 3]);
+        let to_monoid = WrappedFn::from(|x| List::singleton(x));
+        let elem_map = WrappedFn::from(|x| x + 1);
+        let composed = to_monoid.clone().compose(elem_map.clone());
+        let lhs = ListInstance::fold_map(composed, xs.clone());
+        let rhs = ListInstance::fold_map(to_monoid, ListInstance::fmap(elem_map, xs));
+        assert_eq!(lhs, rhs);
+    }
+
+    #[test]
+    fn test_foldable_foldr_to_list_law() {
+        let xs = List::from(vec![1, 2, 3]);
+        let via_foldr =
+            ListInstance::foldr(WrappedFn::curry(List::cons), List::empty(), xs.clone());
+        assert_eq!(via_foldr, xs);
+    }
+}

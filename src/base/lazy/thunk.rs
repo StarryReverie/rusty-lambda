9bf9eec04f54ed607a7f8b92013417896fc73f4d
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-04-13 23:27:31 +0800

    refactor(base): rewrite Thunk evaluation with Computation

--- src/base/lazy/thunk.rs
+++ src/base/lazy/thunk.rs
@@ -3,69 +3,60 @@ use std::hash::{Hash, Hasher};
 use std::ops::Deref;
 use std::sync::{Arc, LazyLock};
 
+use crate::base::computation::{Computation, LazyReturn};
 use crate::base::value::StaticConcurrent;
 
 #[derive(Debug)]
-pub struct Thunk<T>(Arc<ThunkInner<T>>);
+pub struct Thunk<T>(Arc<LazyLock<T, LazyReturn<T>>>);
 
-#[derive(Debug)]
-enum ThunkInner<T> {
-    Evaluated(T),
-    Deferred(LazyLock<Thunk<T>, Box<dyn FnOnce() -> Thunk<T> + Send + Sync + 'static>>),
-}
+impl<T> Thunk<T>
+where
+    T: StaticConcurrent,
+{
+    pub fn new(computation: Computation<T>) -> Self {
+        Self(Arc::new(LazyLock::new(computation.lazy_eval())))
+    }
 
-impl<T> Thunk<T> {
     pub fn immediate(value: T) -> Self {
-        Self(Arc::new(ThunkInner::Evaluated(value)))
+        Self::new(Computation::immediate(value))
     }
 
     pub fn lazy<F>(eval: F) -> Self
     where
         F: FnOnce() -> T + StaticConcurrent,
     {
-        Self::lazy_monadic(move || Self::immediate(eval()))
-    }
-
-    pub fn lazy_monadic<F>(eval: F) -> Self
-    where
-        F: FnOnce() -> Thunk<T> + StaticConcurrent,
-    {
-        let inner = ThunkInner::Deferred(LazyLock::new(Box::new(eval)));
-        Self(Arc::new(inner))
-    }
-
-    pub fn force(&self) -> &T {
-        match self.0.as_ref() {
-            ThunkInner::Evaluated(value) => value,
-            ThunkInner::Deferred(eval) => eval.force(),
-        }
-    }
-
-    pub fn extract(&self) -> T
-    where
-        T: Clone,
-    {
-        self.force().clone()
+        Self::new(Computation::lazy(eval))
     }
 
     pub fn map<U, F>(thunk: Self, func: F) -> Thunk<U>
     where
-        T: StaticConcurrent,
+        U: StaticConcurrent,
         F: FnOnce(&T) -> U + StaticConcurrent,
     {
-        Thunk::lazy(move || func(thunk.force()))
+        Thunk::lazy(move || func(Thunk::force(&thunk)))
     }
 
-    pub fn with<U, F>(thunk: Self, func: F) -> Thunk<U>
+    pub fn bind<U, F>(thunk: Self, func: F) -> Thunk<U>
     where
-        T: StaticConcurrent,
-        F: FnOnce(&T) -> Thunk<U> + StaticConcurrent,
+        U: StaticConcurrent,
+        F: FnOnce(&T) -> Computation<U> + StaticConcurrent,
     {
-        Thunk::lazy_monadic(move || func(thunk.force()))
+        Thunk::new(Computation::monadic(move || func(Thunk::force(&thunk))))
     }
 }
 
-impl<T: Clone> Thunk<T> {}
+impl<T> Thunk<T> {
+    pub fn force(thunk: &Self) -> &T {
+        LazyLock::force(&thunk.0)
+    }
+
+    pub fn extract(thunk: &Self) -> T
+    where
+        T: Clone,
+    {
+        Thunk::force(thunk).clone()
+    }
+}
 
 impl<T> Clone for Thunk<T> {
     fn clone(&self) -> Self {
@@ -73,39 +64,60 @@ impl<T> Clone for Thunk<T> {
     }
 }
 
-impl<T: PartialEq> PartialEq for Thunk<T> {
+impl<T> PartialEq for Thunk<T>
+where
+    T: PartialEq,
+{
     fn eq(&self, other: &Self) -> bool {
-        self.force() == other.force()
+        Thunk::force(self) == Thunk::force(other)
     }
 }
 
 impl<T> Eq for Thunk<T> where T: Eq {}
 
-impl<T: PartialOrd> PartialOrd for Thunk<T> {
+impl<T> PartialOrd for Thunk<T>
+where
+    T: PartialOrd,
+{
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.force().partial_cmp(other.force())
+        Thunk::force(self).partial_cmp(Thunk::force(other))
     }
 }
 
-impl<T: Ord> Ord for Thunk<T> {
+impl<T> Ord for Thunk<T>
+where
+    T: Ord,
+{
     fn cmp(&self, other: &Self) -> Ordering {
-        self.force().cmp(other.force())
+        Thunk::force(self).cmp(Thunk::force(other))
     }
 }
 
-impl<T: Hash> Hash for Thunk<T> {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.force().hash(state);
+impl<T> Hash for Thunk<T>
+where
+    T: Hash,
+{
+    fn hash<H>(&self, state: &mut H)
+    where
+        H: Hasher,
+    {
+        Thunk::force(self).hash(state);
     }
 }
 
-impl<T: Default + StaticConcurrent> Default for Thunk<T> {
+impl<T> Default for Thunk<T>
+where
+    T: Default + StaticConcurrent,
+{
     fn default() -> Self {
         Self::lazy(Default::default)
     }
 }
 
-impl<T> From<T> for Thunk<T> {
+impl<T> From<T> for Thunk<T>
+where
+    T: StaticConcurrent,
+{
     fn from(value: T) -> Self {
         Self::immediate(value)
     }
@@ -115,7 +127,7 @@ impl<T> Deref for Thunk<T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        self.force()
+        Thunk::force(self)
     }
 }
 
